7c69800e582378b40a96dfd4379fcd4cfb3bfb5b
ira <rybakov.i.a.2002@gmail.com>
2024-05-19 11:50:10 +0300

    now nvim config is clean

--- nvim/lua/aliases.lua
+++
@@ -1,5 +0,0 @@
-SetAlias([[Q]], [[q!]])
-SetAlias([[Wq]], [[wq!]])
-SetAlias([[QQ]], [[qa!]])
---SetAlias([[C]], [[w !xclip -selection clipboard]])
-
--- nvim/lua/packages/cool-substitute.lua
+++
@@ -1,3 +0,0 @@
---require([[cool-substitute]]).setup({
---  setup_keybindings = true,
---})
